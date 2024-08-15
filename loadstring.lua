4c0cf6e50febe48b56572fb2854aa32fe0a9982e
carlinhosdoscapa <carlinhosnomatagal208@gmail.com>
2024-08-15 16:20:17 -0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -75,5 +75,5 @@ getgenv().SettingFarm ={
 getgenv().Key = "1400785d3f69359db5dbc06f" 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaCat-kaitunBF.lua"))()
 
-_G.Config = { UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", discord_id = 1037147029614362624 , Note = "[LEVEL MAX] UGPHONE - 2", } 
+_G.Config = { UserID = "2ecc7ebd-4788-47be-b07d-43dd6589ac94", discord_id = 1037147029614362624 , Note = "[LEVEL MAX] UGPHONE - 1", } 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/main/trackstatblox"))()
